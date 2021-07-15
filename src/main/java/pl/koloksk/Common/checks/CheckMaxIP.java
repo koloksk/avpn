85bb1f7ee85140015214f7165562f480617390b8
koloksk <34625628+koloksk@users.noreply.github.com>
2021-07-15 18:17:10 +0200

    Bungee ip limit check
    + add bungee ip limit check

--- src/main/java/pl/koloksk/Common/checks/CheckMaxIP.java
+++ src/main/java/pl/koloksk/Common/checks/CheckMaxIP.java
@@ -1,17 +1,27 @@
 package pl.koloksk.Common.checks;
 
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import pl.koloksk.Bungee.Main;
 import pl.koloksk.Common.utils.Settings;
+import pl.koloksk.Common.utils.StoreData;
 
 public class CheckMaxIP {
 
 
     public static boolean check(String hostname) {
         int clients = 0;
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            if (hostname.equalsIgnoreCase(player.getAddress().getHostName()))
-                clients++;
+        if(!StoreData.bungeeEnabled) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (hostname.equalsIgnoreCase(player.getAddress().getHostName()))
+                    clients++;
+            }
+        } else {
+            for (ProxiedPlayer p : Main.getinstance().getPlayers()){
+                if(hostname.equalsIgnoreCase(p.getAddress().getHostName()))
+                    clients++;
+            }
         }
         return clients >= Settings.maxip_limit;
     }
